ddf3f148e362083cd267ea438c41a37271e6ebd2
Basem Abughallya <31888892+BaZom@users.noreply.github.com>
2022-10-27 14:26:26 +0200

    Update data_quality.py

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -22,4 +22,4 @@ class DataQualityOperator(BaseOperator):
             records = redshift_hook.get_records(check_dict['check_statement'])
             if records[0][0] != check_dict['expected result']:
                 raise ValueError(f"Data quality check failed. {check_dict['table']} contained null values for {check_dict['column']}")
-            self.log.info(f"Data quality on table {check_dict['table']} check passed with not null {check_dict['column']} records")
+            self.log.info(f"Data quality on table {check_dict['table']} check passed; there are no null values in  {check_dict['column']} records")
